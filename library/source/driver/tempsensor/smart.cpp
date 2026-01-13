4d7063ae562cbfd1595428af18444011d61e49b4
David Engen <david.engen722@gmail.com>
2026-01-13 10:00:59 +0100

    add smart implementation details

--- library/source/driver/tempsensor/smart.cpp
+++ library/source/driver/tempsensor/smart.cpp
@@ -1,5 +1,3 @@
-//! @todo Add smart temperature sensor implementation details here!
-
 /**
  * @note Requirements:
  * 
@@ -13,3 +11,64 @@
  *           - The ADC must be initialized.
  *           - The linear regression model must be pre-trained.
  */
+
+ /**
+ * @brief Smart temperature sensor implementation.
+ */
+#include <stdint.h>
+
+//Inkludera ADc interface ml/lin_reg inteface och utility-funktioner
+#include "driver/adc/interface.h"       // Contains ADC interface.
+#include "driver/tempsensor/smart.h"    // Contains the smart sensor class.
+#include "ml/lin_reg/interface.h"       // Contains the liniar regression interface.
+#include "utils/utils.h"                // Contains a function to round numbers.
+
+namespace driver
+{
+namespace tempsensor
+{
+/**
+ * @brief Smart temperature sensor implementation.
+ */
+class Smart final : public Interface
+{
+public:
+//------------------------------------------------------------------------------
+Smart(uint8_t pin, adc::Interface& adc, ml::lin_reg::Interface& linReg) noexcept
+    : myAdc{adc}
+    , myLinReg{linReg}
+    , myPin{pin}
+{
+    // Enable the ADC if initialization succeded.
+    if (isInitialized()) { myAdc.setEnabled(true); }
+}
+
+//------------------------------------------------------------------------------
+bool Smart::isInitialized() const noexcept
+{
+    // Return True if the ADC is initialized, the temp sensor pin is a valid ADC channel
+    // and the linear regression model trained.
+    return myAdc.isInitialized() && myAdc.isChannelValid(myPin) && myLinReg.isTrained(); 
+}
+
+//------------------------------------------------------------------------------
+int16_t read() const noexcept
+{
+    // Read the tmeperature if the temp sensor is initialized.
+    if (isInitialized())
+    {
+        // Calculate the input voltage
+        const double inputVoltage{myAdc.inputVoltage(myPin)};
+        // Predict the temperature based on the input voltage
+        const double predictedTemp{myLinReg.predict(inputVoltage)};
+        // Predict the temperature rounded to the nearest interger.
+        return utils::round<int16_t>(predictedTemp);
+    }
+
+    // Return 0 if the temp sensor isn't initialized.
+    return 0;
+}
+
+};
+} // namespace tempsensor
+} // namespace driver
